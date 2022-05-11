6582d52890ceb6e4379a85f8d473f0c00b5b6539
Siqi Wang <s.wang@uci.edu>
2022-05-10 17:56:54 -0700

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Siqi Wang
+# @Date:   2022-05-10 17:56:29
+# @Last Modified by:   Siqi Wang
+# @Last Modified time: 2022-05-10 17:56:31
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
